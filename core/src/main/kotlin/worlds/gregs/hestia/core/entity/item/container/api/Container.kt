70ac3a013c826ef908bb5353651c520a33e4532c
GregHib <gregoryhib@gmail.com>
2020-01-20 22:11:58 +0000

    WIP

--- core/src/main/kotlin/worlds/gregs/hestia/core/entity/item/container/api/Container.kt
+++ core/src/main/kotlin/worlds/gregs/hestia/core/entity/item/container/api/Container.kt
@@ -3,6 +3,7 @@ package worlds.gregs.hestia.core.entity.item.container.api
 import arrow.core.Either
 import worlds.gregs.hestia.core.entity.item.container.model.Item
 import worlds.gregs.hestia.core.entity.item.container.model.StackType
+import worlds.gregs.hestia.core.task.api.TaskCancellation
 import worlds.gregs.hestia.service.cache.DefinitionReader
 import worlds.gregs.hestia.service.cache.definition.definitions.ItemDefinition
 
@@ -10,3 +11,23 @@ data class ContainerModificationDetails(val either: Either<ItemResult.Issue, Con
 typealias Composition = (ContainerModificationDetails) -> ContainerModificationDetails
 typealias Container = Array<Slot>
 typealias Slot = Item?
+
+/**
+ * Checks the parameters match the values in the container
+ */
+fun Container.validateItem(slot: Int, type: Int): Item? {
+    val item = getOrNull(slot)
+    if(item == null && type != -1) {
+        throw TaskCancellation.Cancellation("Null item container item $slot $type")
+    } else if(item != null && item.type != type) {
+        throw TaskCancellation.Cancellation("Invalid item container item $slot $type - $item")
+    }
+    return item
+}
+
+/**
+ * Checks the slot value isn't empty
+ */
+fun Container.validateSlot(slot: Int): Item {
+    return getOrNull(slot) ?: throw TaskCancellation.Cancellation("Null item container slot $slot")
+}
